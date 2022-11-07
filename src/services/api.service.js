126f81f0760bf65fcb95d706959a5d4986f03b8c
solazy01 <73142849+solazy01@users.noreply.github.com>
2022-11-07 22:54:02 +0200

    Update api.service.js

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://youtube-music1.p.rapidapi.com",
   headers: {
-    "X-RapidAPI-Key": "590342ec98mshca2893ee0d1c529p113b71jsn1b113c7f70d5",
+    "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
     "X-RapidAPI-Host": "youtube-music1.p.rapidapi.com",
   },
 });
